e1bdfffc0103d536a6d7ce5f4d3c506228af6f36
Connor <conkelley7@gmail.com>
2025-02-05 21:04:08 -0600

    Set up Middleware to allow Frontend-Backend Communication. Only Localhost for now.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Middleware configuration for Frontend-Backend communication
+app.add_middleware(
+    CORSMiddleware,
+
+    allow_origins=[
+        # Only localhost for now
+        "http://localhost:8000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def root():
+    return {"root": "test"}
 
